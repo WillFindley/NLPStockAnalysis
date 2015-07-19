e18ece825a83a4846865d589ac4f5dfefe8ead57
Will Findley <findley@gmail.com>
2015-07-19 17:52:37 -0400

    parse history and plots

--- ChartCorrelations.py
+++ ChartCorrelations.py
@@ -1,4 +1,15 @@
-from lxml import html
+import pylab as plot
+import pandas as pd
+import numpy as np
 import requests
 
+def getStockHistoryCSV(whichNYSE):
+    page = requests.get('http://finance.yahoo.com/q/hp?s=' + whichNYSE + '+Historical+Prices')
+    url = 'http://real-chart.finance.yahoo.com/table.' + page.text[page.text.find('csv'):page.text.rfind('csv')+3]
+    stockHistory = pd.read_csv(url)
+    stockHistory['Date'] = pd.to_datetime(stockHistory['Date'])
+    return stockHistory
 
+
+def plotCorrelations(stockHistory):
+    pd.DataFrame.plot(stockHistory,x='Date',y='Close')
