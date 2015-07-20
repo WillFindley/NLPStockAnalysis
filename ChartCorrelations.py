8db237ae3ef0bc9c77786c0edf020411ee657287
Will Findley <findley@gmail.com>
2015-07-20 04:07:24 -0400

    expert working

--- ChartCorrelations.py
+++ ChartCorrelations.py
@@ -22,6 +22,7 @@ def getStockHistoryCSV(whichNYSE,allStratsRaw):
     stockHistory.drop([u'Open', u'High', u'Low', u'Close', u'Volume'], inplace=True, axis=1)
     stockHistory['Date'] = pd.to_datetime(stockHistory['Date'])
     stockHistory.columns = [u'Date', unicode(whichNYSE,"utf-8")]
+    stockHistory = stockHistory.iloc[::-1]
     allStratsRaw[whichNYSE] = stockHistory
     return allStratsRaw
 
@@ -29,7 +30,7 @@ def getStockHistoryCSV(whichNYSE,allStratsRaw):
 def getExpertStrategy(whichNYSE,allStratsRaw):
 
     MonthConversion = {'Jan':1, 'Feb':2, 'Mar':3, 'Apr':4, 'May':5, 'Jun':6, 'Jul':7, 'Aug':8, 'Sep':9, 'Oct':10, 'Nov':11, 'Dec':12}
-    ActionConversion = {'Buy':1, 'Underperform':0, 'Sector Perform':0.5, 'Neutral':0.5, 'Outperform':1, 'Mkt Underperform':0, 'Mkt Perform':0.5, 'Hold':0.5, 'Perform':0.5, 'Accumulate':1, 'Sell':0, 'In Line':0.5, 'Strong Buy':1, 'Overweight':1, 'Sector Outperform':1, 'Equal-weight':0.5, "Market Perform": 0.5, "NT Strong Buy":1, "NT Buy":1, "Attractive":1, "Mkt Outperform":1, "NT Neutral":0.5, "NT Accum":1, "LT Buy":1, "Reduce":0, "NT Accumulate":1, "Perform In Line":0.5}
+    ActionConversion = {'Buy':1, 'Underperform':0, 'Sector Perform':0.5, 'Neutral':0.5, 'Outperform':1, 'Mkt Underperform':0, 'Mkt Perform':0.5, 'Hold':0.5, 'Perform':0.5, 'Accumulate':1, 'Sell':0, 'In Line':0.5, 'Strong Buy':1, 'Overweight':1, 'Sector Outperform':1, 'Equal-weight':0.5, "Market Perform": 0.5, "NT Strong Buy":1, "NT Buy":1, "Attractive":1, "Mkt Outperform":1, "NT Neutral":0.5, "NT Accum":1, "LT Buy":1, "Reduce":0, "NT Accumulate":1, "Perform In Line":0.5, 'Average':0.5, 'Source of Funds':0, 'Above Average':1, 'Over Weight':1, 'Equal Weight':0.5, 'Peer Perform':0.5, 'Positive':1, 'In-Line':0.5, 'In-line':0.5, 'LT Neutral':0.5, 'Add':1, 'Maintain Position':0.5, 'NT Accum/LT Accum':1, 'Top Pick':1, 'Recomm. List':1, 'LT Attractive':1}
 
     expertWeights = []
 
@@ -59,39 +60,63 @@ def getExpertStrategy(whichNYSE,allStratsRaw):
     return expertWeights
 
 
-def condenseStrategyData(allStratsRaw,expertWeights):
+def condenseStrategyData(allStratsRaw,whichNYSE,expertWeights):
     keys = list(allStratsRaw.keys())
     condensedData = allStratsRaw[keys.pop()]
     while len(keys) > 0:
         toJoinData = allStratsRaw[keys.pop()]
         condensedData = pd.merge(condensedData, toJoinData, how='inner', on='Date')
     condensedData['Expert'] = pd.Series(np.random.randn(len(condensedData['Date'])), index=condensedData.index)
-    condensedData = fillExpert(condensedData,expertWeights)
+    condensedData = fillExpert(condensedData,whichNYSE,expertWeights)
     condensedData = condensedData.set_index(u'Date')
     return condensedData
 
 
-def fillExpert(condensedData,ExpertWeights):
+def fillExpert(condensedData,whichNYSE,expertWeights):
+   
+    whichWeight = len(expertWeights) - 1
+    amountDJIA = 0.5
+    amountNYSE = 0.5
+    condensedData['Expert'][0] = amountDJIA + amountNYSE
+    for row in xrange(1,len(condensedData['Expert'])):
 
-    
+        # redistribute investment based on expert opinion
+        if whichWeight >= 0 and condensedData['Date'][row] >= expertWeights[whichWeight-1][0]:
+            whichWeight -= 1
+            if expertWeights[whichWeight][1] == 1:
+                amountNYSE += amountDJIA
+                amountDJIA = 0
+            elif expertWeights[whichWeight][1] == 0:
+                amountDJIA += amountNYSE
+                amountNYSE = 0
+            else:
+                amountNYSE = (amountNYSE+amountDJIA)/2
+                amountDJIA = amountNYSE
+
+        amountNYSE *= condensedData[whichNYSE][row] / condensedData[whichNYSE][row-1]
+        amountDJIA *= condensedData['DJIA'][row] / condensedData['DJIA'][row-1]
+
+        condensedData['Expert'][row] = amountNYSE + amountDJIA
+
+    return condensedData
 
 
 def plotCorrelations(condensedData):
     keys = list(condensedData.columns)
     # start all strategies at $1
     for key in keys:
-        condensedData[key] = condensedData[key] / condensedData[key][-1]
+        condensedData[key] = condensedData[key] / condensedData[key][0]
     condensedData.plot()
     plt.xlabel('Date')
     plt.ylabel('Money')
     plt.show()
 
 
-#allStratsRaw = {}
-#pathToDJIACSV = 'DJIA.csv'
-#allStratsRaw = getDJIAHistoryCSV(pathToDJIACSV,allStratsRaw)
-whichNYSE = 'BAC'
-#allStratsRaw = getStockHistoryCSV(whichNYSE,allStratsRaw)
-#expertWeights = getExpertStrategy(whichNYSE,allStratsRaw)
-condensedData = condenseStrategyData(allStratsRaw,expertWeights)
-#plotCorrelations(condensedData)
+allStratsRaw = {}
+pathToDJIACSV = 'DJIA.csv'
+allStratsRaw = getDJIAHistoryCSV(pathToDJIACSV,allStratsRaw)
+whichNYSE = 'AAPL'
+allStratsRaw = getStockHistoryCSV(whichNYSE,allStratsRaw)
+expertWeights = getExpertStrategy(whichNYSE,allStratsRaw)
+condensedData = condenseStrategyData(allStratsRaw,whichNYSE,expertWeights)
+plotCorrelations(condensedData)
