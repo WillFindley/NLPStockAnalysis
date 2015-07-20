02ef1739d81a4ce4006acadbdc32ca6816c8c015
Will Findley <findley@gmail.com>
2015-07-19 22:16:13 -0400

    working on join

--- ChartCorrelations.py
+++ ChartCorrelations.py
@@ -1,21 +1,48 @@
-import pylab as plot
+import pylab as plt
 import pandas as pd
 import numpy as np
 from scipy import stats
 import requests
 
-def getStockHistoryCSV(whichNYSE):
+
+def getDJIAHistoryCSV(pathToDJIACSV,allStratsRaw):
+    indexHistory = pd.read_csv(pathToDJIACSV)
+    indexHistory['DATE'] = pd.to_datetime(indexHistory['DATE'])
+    indexHistory.columns = [u'Date', u'DJIA']
+    allStratsRaw['DJIA'] = indexHistory
+    return allStratsRaw
+
+
+def getStockHistoryCSV(whichNYSE,allStratsRaw):
     page = requests.get('http://finance.yahoo.com/q/hp?s=' + whichNYSE + '+Historical+Prices')
     url = 'http://real-chart.finance.yahoo.com/table.' + page.text[page.text.find('csv'):page.text.rfind('csv')+3]
     stockHistory = pd.read_csv(url)
+    stockHistory.drop([u'Open', u'High', u'Low', u'Close', u'Volume'], inplace=True, axis=1)
     stockHistory['Date'] = pd.to_datetime(stockHistory['Date'])
-    return stockHistory
+    stockHistory.columns = [u'Date', unicode(whichNYSE,"utf-8")]
+    allStratsRaw[whichNYSE] = stockHistory
+    return allStratsRaw
+
+
+def condenseStrategyData(allStratsRaw):
+    keys = list(allStratsRaw.keys())
+    condensedData = allStratsRaw[keys.pop()]
+    while len(keys) > 0:
+        toJoinData = allStratsRaw[keys.pop()]
+        condensedData = merge(condensedData, toJoinData, how='inner', on='Date')
 
 
 def plotCorrelations(stockHistory):
-    x = stockHistory['Date'][0:-1].values
-    y = stats.mstats.zscore(stockHistory['Adj Close'][0:-1].values - stockHistory['Adj Close'][1:].values) / stockHistory['Adj Close'][1:].values
-    plot.plot(x,y)
-    plot.xlabel('Date')
-    plot.ylabel('% Change Close-To-Close')
-    plot.show()
+    dates = stockHistory['Date'].values
+    openingOpen = stockHistory['Adj Close'].values[-1]
+    stockAlone = stockHistory['Adj Close'].values / openingOpen
+    moneyAlone = np.array([1 for i in xrange(len(stockAlone))])
+    holdMoney, = plt.plot(dates, moneyAlone, label='Hold Money')
+    holdStock, = plt.plot(dates, stockAlone, label='Hold Stock')
+    plt.legend(handles=[holdMoney, holdStock])
+    plt.xlabel('Date')
+    plt.ylabel('Money')
+    plt.show()
+
+
+allStratsRaw = {}
